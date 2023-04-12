a4b20c214d416e1c0fa982828a2012327477634a
chen1238 <65192478+chen1238@users.noreply.github.com>
2023-04-13 00:04:12 +0800

    Update 1.sh

--- introgression/ABBABABAwindows/1.sh
+++ introgression/ABBABABAwindows/1.sh
@@ -1,1 +1,6 @@
-python parseVCF.py -i input.vcf.gz | bgzip > output.geno.gz
+#format
+python ../VCF_processing/parseVCF.py -i cas.recode.vcf |bgzip > cas.geno.gz
+#ABBABABA-D
+python ../ABBABABAwindows.py -g ./cas.geno.gz -f phased -o cas.csv -w 100000 -s 20000 -m 100 -T 10 -P1 c -P2 h -P3 m -O o --popsFile cas.txt --writeFailedWindows 
+#fst,dxy
+python ../popgenWindows.py -g ./cas.geno.gz -f phased -o cas-pop.csv -w 100000 -s 20000 -m 100 -T 10 -p c -p h -p m -p o --popsFile cas.txt --writeFailedWindows 
